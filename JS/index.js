1a7c9c37103f0c1b051f3081fea462072ae30c84
Lukasz Grzegorzewski <grzegorzewski.luk@gmail.com>
2022-10-05 14:56:02 +0200

    Lukasz_index.js moving cards depending on their height

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,3 @@
-
-
 fetch("../pages/header.html")
   .then(response => {
     return response.text()
@@ -8,12 +6,30 @@ fetch("../pages/header.html")
     document.querySelector("header").innerHTML = data;
   })
   .then(() => {
+    //-------------------BURGER CLICK-------------------
     const menuBurger = document.querySelector(".menu-burger");
     const navLinks = document.querySelector(".nav-links")
 
     menuBurger.addEventListener("click", () => {
       navLinks.classList.toggle('menu-burger')
     })
+
+    //----------CARD EFFECT LISTANER -------------------
+
+    const cardDiv = document.querySelectorAll('.card');
+    window.addEventListener('scroll', () => {
+      const { scrollTop, clientHeight } = document.documentElement;
+      
+      for (let i = 0; i < cardDiv.length; i++) {
+
+        if (scrollTop > (scrollTop + cardDiv[i].getBoundingClientRect().top).toFixed() - clientHeight * 0.7) {
+          cardDiv[i].classList.add('card-effect');
+        } else {
+          cardDiv[i].classList.remove('card-effect');
+        }
+      }
+    });
+
   });
 
 fetch("../pages/footer.html")
@@ -24,4 +40,3 @@ fetch("../pages/footer.html")
     document.querySelector("footer").innerHTML = data;
   });
 
-  
