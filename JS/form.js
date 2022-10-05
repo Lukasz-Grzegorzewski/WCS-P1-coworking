bbee4ac499aa644cee6832b330a000aa20e56f55
Adam Giromanay <adam202009@live.fr>
2022-10-05 11:56:27 +0200

    Adam form avec un peu de js

--- JS/form.js
+++ JS/form.js
@@ -8,11 +8,11 @@ fetch("../pages/header.html")
     .then(() => {
         const menuBurger = document.querySelector(".menu-burger");
         const navLinks = document.querySelector(".nav-links")
-    
+
         menuBurger.addEventListener("click", () => {
-          navLinks.classList.toggle('menu-burger')
+            navLinks.classList.toggle('menu-burger')
         })
-      });
+    });
 
 fetch("../pages/footer.html")
     .then(response => {
@@ -21,3 +21,12 @@ fetch("../pages/footer.html")
     .then(data => {
         document.querySelector("footer").innerHTML = data;
     });
+
+const form = document.querySelector("#form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+
+form.onsubmit = function (event) {
+    event.preventDefault();
+    console.log(`Salut ${name.value} merci pour ton message nous te recontacterons sur ${email.value} dès que nos equipes l'auront traité !`);
+};
